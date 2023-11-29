7575c764027efe7fe77f6105e5a0a7ec035e3f51
shinyyxxx <puriwat888@gmail.com>
2023-11-29 12:14:47 +0700

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,6 +37,7 @@ function App() {
         <Route path='/enrollment' element = {<EnrollmentPage />} />
         <Route path='/booking' element = {<BookingPage />} />
       </Routes>
+      <p></p>
     </UserContextProvider>
   )
 }
